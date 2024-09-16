49a1a99fcf4d7848b887b034bd354e19bb0920f9
relate <relatemvp.v2@gmail.com>
2024-09-16 21:01:31 +0200

    react bootstrap integration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from './features/auth/Login'; 
 import LanguageSwitcher from './components/LanguageSwitcher'; 
+import LandingPage from './components/langingPage/LandingPage';
 
 function App() {
    
   return (
     <div className="App">
-      <LanguageSwitcher />
-      
-
+    {/*   <LanguageSwitcher />
       <h1>Welcome to the Authentication Page</h1>
       <div className="authentication-section">
         <Login /> 
-
-      </div>
+      </div> */}
+           <LandingPage/>
     </div>
   );
 }
