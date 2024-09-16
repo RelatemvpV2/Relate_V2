2692dd1873656a2298e65c9df09e82188a76a0f6
relate <relatemvp.v2@gmail.com>
2024-09-16 21:02:15 +0200

    Navbar Component

---
+++ src/components/langingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../navigation_langingPage/NavBar'
+import { Container } from 'react-bootstrap'
+
+const LandingPage = () => {
+  return (
+    <Container fluid className='p-0 m-0'>
+        <NavBar/>
+    </Container>
+  )
+}
+
+export default LandingPage
