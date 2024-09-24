9b33b75b48985981365c4ca781a32dcf121dbc79
relate <relatemvp.v2@gmail.com>
2024-09-24 22:25:07 +0200

    Landing page with out responsive design

---
+++ src/components/dividerContainer/Divider.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Divider = () => {
+  return (
+    <div className="dividercontainer">
+          <div className="svg-container">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="2"
+              height="308"
+              viewBox="0 0 2 308"
+              fill="none"
+            >
+              <path d="M1 0V308" stroke="#41414E" />
+            </svg>
+          </div>
+        </div>
+  )
+}
+
+export default Divider
