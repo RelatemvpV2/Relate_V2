c3d483149b9e95792ff1a3d4a0de50af2e9ae4ea
relate <relatemvp.v2@gmail.com>
2024-11-11 20:06:24 +0100

    receive invitation messages screen

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import InviteCreateUser from "./features/userInvite/InviteCreateUser";
 import StartQuesPage from "./features/startQuestionare/StartQuesPage";
 import LanguageSwitcher from "./components/LanguageSwitcher";
-import Assessment from "./features/assessment/Assessment"; 
+import Assessment from "./features/assessment/Assessment";
 import LandingPage from "./components/landingPage/LandingPage";
 import PageNotFound from "./components/pageNotFound/PageNotFound";
 import PopUpComponent from "./components/popUp/PopUpComponent";
@@ -20,44 +20,50 @@ import ComparedAnswers from "./components/comparedAnswers/ComparedAnswers";
 import SubscriptionsPage from "./components/subscription/SubscriptionsPage";
 
 import { AuthProvider } from "./components/logout/AuthContext";
+import ReceiveInvite from "./components/receiveInvitepage/ReceiveInvite";
+
 function App() {
   return (
-    
+
 
     <Router>
       <AuthProvider>
-      <Routes>
+        <Routes>
 
-        <Route path="/" element={<LandingPage />} />
-        {/* Define the route for the login page */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword/>}/>
-        <Route path={`/reset-password`} element={<ResetPassword/>}/>
+          <Route path="/" element={<LandingPage />} />
+          {/* Define the route for the login page */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path={`/reset-password`} element={<ResetPassword />} />
+          <Route path="/popup" element={<PopUpComponent />} />
 
-        <Route path="/popup" element={<PopUpComponent />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        {/* Define the route for InviteCreateUser page */}
-        <Route path="/userInvite/InviteCreateUser" element={<InviteCreateUser />} />
-        <Route path="/startQuestionare/StartQuesPage" element={<StartQuesPage />} />
-        <Route path="/assessment/Assessment" element={<Assessment />} />
-        <Route path="/level1/yourSummary" element={<Summary1stUser />} />
-        <Route path="/level1/invitePartner2" element={<InvitePartner2 />} />
-        <Route path="/level1/waitingParnerResponse" element={<WaitingForPartnerResonse1 />} />
-        <Route path="/level1/comparedResults" element={<ComparedAnswers/>} /> 
-        <Route path="/level1/subscriptions" element={<SubscriptionsPage/>} />
-
-        {/* Define the route for Page not found */}
-        <Route
-          path="*"
-          element={<PageNotFound />}
-        />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/messages" element={<ReceiveInvite />} />
 
 
-      </Routes>
+          {/* Define the route for InviteCreateUser page */}
+          <Route path="/userInvite/InviteCreateUser" element={<InviteCreateUser />} />
+          <Route path="/startQuestionare/StartQuesPage" element={<StartQuesPage />} />
+          <Route path="/assessment/Assessment" element={<Assessment />} />
+          <Route path="/level1/yourSummary" element={<Summary1stUser />} />
+          <Route path="/level1/invitePartner2" element={<InvitePartner2 />} />
+          <Route path="/level1/waitingParnerResponse" element={<WaitingForPartnerResonse1 />} />
+          <Route path="/level1/comparedResults" element={<ComparedAnswers />} />
+          <Route path="/level1/subscriptions" element={<SubscriptionsPage />} />
+
+
+          {/* Define the route for Page not found */}
+          <Route
+            path="*"
+            element={<PageNotFound />}
+          />
+
+
+        </Routes>
       </AuthProvider>
     </Router>
-    
+
 
   );
 }
