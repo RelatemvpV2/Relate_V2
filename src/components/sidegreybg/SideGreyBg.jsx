6c3f6607a3d2733cc601513cf80a6ad201c5a178
relate <relatemvp.v2@gmail.com>
2024-11-18 12:12:40 +0100

    left nav bar

--- src/components/sidegreybg/SideGreyBg.jsx
+++ src/components/sidegreybg/SideGreyBg.jsx
@@ -17,6 +17,10 @@ const email = window.localStorage.getItem("email");
 
 const SideGreyBg = () => {
   const [showRelations, setShowRelations] = useState(false);
+  const [showSubMe, setShowSubMe] = useState(false);
+
+
+
   const [messages, setMessages] = useState([])
 
   const navigate = useNavigate();
@@ -24,6 +28,10 @@ const SideGreyBg = () => {
   const toggleRelations = () => {
     setShowRelations((prevShowRelations) => !prevShowRelations);
   };
+
+  const toggleMe = () => {
+    setShowSubMe((prevShowMe) => !prevShowMe);
+  }
 
   const redirectToInvitations = () => {
     navigate("/dashboard/messages");
@@ -33,14 +41,8 @@ const SideGreyBg = () => {
     navigate("/dashboard")
   }
 
- /*  const userRelations = {
-
-    invitationNotSent: ['Sample Person 1'],
-    waitingForResponse: ['Sample Person 2'],
-  };
- */
   const checkPendingInvitations = messages && messages.filter((msg) => msg.invitation_status === 'pending')
-  
+
 
   useEffect(() => {
     const fetchPartnerEmail = async () => {
@@ -57,7 +59,7 @@ const SideGreyBg = () => {
     fetchPartnerEmail();
   }, []);
 
-  console.log("messages side bar",messages)
+  console.log("messages side bar", messages)
 
   return (
     <div className="sidegrey-bg">
@@ -66,11 +68,19 @@ const SideGreyBg = () => {
 
 
         <nav className="sidebar-nav">
-          <Text type="p" className="therapist-text"
-            onClick={redirectToInvitations}>
-            Me
-
+          <Text type="p" className={`Me-text ${showSubMe ? 'active' : ''}`}
+            onClick={toggleMe}
+          >
+            -Me-
           </Text>
+          {
+            showSubMe && (<>
+              <Text type='p' className='relation-item' onClick={redirectToInvitations}>Messages</Text>
+              <Text type='p' className='relation-item'>Sub Menu</Text>
+            </>
+
+            )
+          }
           <svg xmlns="http://www.w3.org/2000/svg" width="173" height="2" viewBox="0 0 173 2" fill="none">
             <path d="M0 1H173" stroke="#F9EEE1" stroke-width="0.5" />
           </svg>
@@ -84,40 +94,39 @@ const SideGreyBg = () => {
           {showRelations && (
             <div className="relations-dropdown">
               {messages
-              .filter(each => each.invitation_status === "Accepted")
-              .map((relations, i) => (
-                <Text key={i} type="p" className="relation-item"
-                  onClick={() => redirectToRelation(relations)}>
-                  {relations.sender_name}
-                </Text>
-              ))}
-            </div>
-          )}
-          {/* {userRelations.invitationNotSent.length > 0 && ( */}
-          {(messages && messages.length > 0 && messages.filter(each => each.sender_email === email) ) && (
+                .filter(each => each.invitation_status === "Accepted")
+                .map((relations, i) => (
+                  <Text key={i} type="p" className="relation-item"
+                    onClick={() => redirectToRelation(relations)}>
+                    {relations.sender_name}
+                  </Text>
+                ))}
+              {/* {userRelations.invitationNotSent.length > 0 && ( */}
+              {(messages && messages.length > 0 && messages.filter(each => each.sender_email === email)) && (
 
-            <div className="category">
-              <Text type="p" className="relation-item active">No inviation sent</Text>
-            </div>
-          )}
+                <div className="category">
+                  <Text type="p" className="relation-item active">No new inviation sent</Text>
+                </div>
+              )}
 
 
-          {/* {userRelations.waitingForResponse.length > 0 && ( */}
-          {(messages && messages.length > 0 && checkPendingInvitations.length > 0 ) && (
+              {/* {userRelations.waitingForResponse.length > 0 && ( */}
+              {(messages && messages.length > 0 && checkPendingInvitations.length > 0) && (
 
-            <div className="category">
-              <Text type="p" className="relation-item active">
-                Waiting for relation
+                <div className="category">
+                  <Text type="p" className="relation-item active">
+                    Waiting for relation
+                  </Text>
+                </div>
+              )}
+             
+              <Text type="p" className="newrelation-text">
+                + Add new relation
+
               </Text>
             </div>
           )}
-         {/*  <Text type="p" className="myrelations-text">
-            James S
-          </Text> */}
-          <Text type="p" className="newrelation-text">
-            + Add new relation
 
-          </Text>
           <svg xmlns="http://www.w3.org/2000/svg" width="173" height="2" viewBox="0 0 173 2" fill="none">
             <path d="M0 1H173" stroke="#F9EEE1" stroke-width="0.5" />
           </svg>
