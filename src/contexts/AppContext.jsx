29bd7bb889dda3c5e02c536545f03e30fdb60de6
relate <relatemvp.v2@gmail.com>
2024-11-29 09:32:06 +0100

    context working - selecting the parner & display name in Dashboard

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,12 +1,16 @@
-import React, { createContext } from 'react';
+import React, { createContext , useState} from 'react';
 
-const AppContext = createContext();
+export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
- 
+ const [current_Relation,setCurrent_Relation] = useState()
+
+/*  const updateCR = (relation) => {
+  setCurrent_Relation(relation)
+ } */
 
     return (
-      <AppContext.Provider value={{}}>
+      <AppContext.Provider value={{ current_Relation,  setCurrent_Relation }}>
         {children}
       </AppContext.Provider>
     );
